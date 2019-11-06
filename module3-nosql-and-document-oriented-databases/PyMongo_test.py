ef69c1d55719ce319072a147bef957967e0aac79
CurtCalledBurt <CurtCalledBurt@users.noreply.github.com>
2019-11-06 14:19:40 -0800

    added and rpg_pymongo.py file which adds data from a dummy rpg dataset into the pymongo database.

--- module3-nosql-and-document-oriented-databases/PyMongo_test.py
+++ module3-nosql-and-document-oriented-databases/PyMongo_test.py
@@ -30,6 +30,7 @@ multiple line long string.
 
 import pymongo 
 
+client = pymongo.MongoClient("mongodb://admin:HemG8m35dwrcsJtY@cluster0-shard-00-00-q6cbk.mongodb.net:27017,cluster0-shard-00-01-q6cbk.mongodb.net:27017,cluster0-shard-00-02-q6cbk.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.test
 
 # Count how many documents
